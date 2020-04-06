d947628078609cea5f0c383f391fb9d4bad42c04
Liam T <liam.t10@icloud.com>
2020-04-06 18:51:26 +0100

    working on nesting and averages

--- src/index.js
+++ src/index.js
@@ -17,6 +17,41 @@ const client = new MongoClient(url, {
     const forceNames = await byQuarterCollection
       .aggregate([{ $group: { _id: 0, names: { $addToSet: '$forceName' } } }])
       .toArray();
+    const regionOverview = await byQuarterCollection.aggregate([
+      { $group: {
+        _id: {
+          region: '$Region',
+          financialYear: '$financialYear',
+          // knifeEnabled: '$knifeEnabled',
+        },
+        knifeEnabled: { $avg: '$knifeEnabled' },
+      } },
+      { $sort: { 'knifeEnabled': 1 } },
+      {
+        $replaceRoot: {
+          newRoot: {
+            name: { $concat: ['$_id.region', ' - ', '$_id.financialYear'] },
+            averageKnifeEnabled: '$knifeEnabled',
+          },
+        },
+      },
+      // { $group: {
+      //   _id: '$_id.region',
+      //   years: { $addToSet: {
+      //     name: '$_id.financialYear',
+      //     knifeEnabled: { $sum: '$_id.knifeEnabled' },
+      //   } },
+      //   // year: { $sum: '$_id.knifeEnabled' },
+      // } },
+      // { $sort: { '_id.financialYear': 1 } },
+      // { $group: {
+      //   _id: '$_id.region',
+      //   years: { $push: {
+      //     year: '$_id.financialYear',
+      //     knifeEnabled: '$_id.knifeEnabled',
+      //   } },
+      // } },
+    ]).toArray();
 
     const clevelandGroup = await byQuarterCollection
       .aggregate([
@@ -40,7 +75,8 @@ const client = new MongoClient(url, {
       .toArray();
 
     // console.log('forceNames: %o', forceNames);
-    console.log('clevelandGroup: %o', clevelandGroup);
+    // console.log('clevelandGroup: %o', clevelandGroup);
+    console.log('regionOverview: %o', regionOverview);
     // console.log('records: %o', records);
   } catch ({ stack }) {
     console.error(stack);
